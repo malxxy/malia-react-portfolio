2e62dc9b4d50ea2704373b09c6c91ed985fb91df
malxxy <maliayraguen@gmail.com>
2023-04-05 12:21:11 -0700

    layout pages'
    gi

---
+++ malia-portfolio/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const styles = {
+  headerStyle: {
+    background: 'black',
+  },
+  headingStyle: {
+    fontSize: '100px',
+    color: 'white',
+  },
+};
+
+function Header() {
+  return (
+    <header style={styles.headerStyle} className="header">
+      <h1 style={styles.headingStyle}>Malia Yraguen</h1>
+    </header>
+  );
+}
+
+export default Header;
